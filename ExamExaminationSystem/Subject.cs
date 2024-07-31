9bd81fcca2fddfba4ba01ceb0116fa2b25fed4d7
MustafaSalah <56977391+MustafaSalahX@users.noreply.github.com>
2024-07-31 16:59:20 +0300

    Added New File

---
+++ ExamExaminationSystem/Subject.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamExaminationSystem
+{
+    public class Subject
+    {
+        public int ID { get; set; }
+        public String Name { get; set; }
+
+
+
+
+
+    }
+}
